d4a6466035fa98625dd96b75970e75e8a4aea288
Maxim Samburskiy <55308117+alpacone@users.noreply.github.com>
2022-06-22 23:02:22 +0500

    Multiple files support

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const { xml2js, js2xml } = require('xml-js');
 const { flattie } = require('flattie');
 const { nestie } = require('nestie');
 const prettyHrtime = require('pretty-hrtime');
+const { mkdir } = require('mk-dirs/sync');
+
+mkdir('exemel');
 
 const startAt = process.hrtime();
 
@@ -11,41 +14,38 @@ const startAt = process.hrtime();
 const args = process.argv.slice(2);
 console.log('Args:', args);
 
-// Узнаём полный путь до файла
-const filePath = args[0];
-console.log('File path:', filePath);
+for (const filePath of args) {
+  // Узнаём полный путь до файла
+  console.log(filePath);
 
-// читаем файл и парсим его
-const content = fs.readFileSync(filePath, 'utf-8');
-const json = xml2js(content, { alwaysChildren: true, ignoreComment: true });
+  // читаем файл и парсим его
+  const content = fs.readFileSync(filePath, 'utf-8');
+  const json = xml2js(content, { alwaysChildren: true, ignoreComment: true });
+  const flat = flattie(json, '.', true);
 
-// разбираем файл
-console.log('Transforming...');
-const flat = flattie(json, '.', true);
+  // основная логика
+  for (const [key, value] of Object.entries(flat)) {
+    const isZ = key.endsWith('.attributes.z');
+    if (!isZ) continue;
 
-// основная логика
-for (const [key, value] of Object.entries(flat)) {
-  const isZ = key.endsWith('.attributes.z');
-  if (!isZ) continue;
+    const base = key.slice(0, -13);
+    const isPos = flat[base + '.name'] === 'position';
+    if (isPos && isZ) flat[key] = (+value + 50).toFixed(8);
+  }
 
-  const base = key.slice(0, -13);
-  const isPos = flat[base + '.name'] === 'position';
-  if (isPos && isZ) flat[key] = (+value + 50).toFixed(8);
+  // собираем файл обратно
+  const obj = nestie(flat);
+  const xml = js2xml(obj, { spaces: 2 });
+
+  // узнаём полный файл для будущего файла
+  const pathObject = path.parse(filePath);
+  pathObject.dir = path.join(pathObject.dir, 'exemel');
+  const newFilePath = path.format(pathObject);
+
+  // записываем файл
+  fs.writeFileSync(newFilePath, xml, 'utf-8');
 }
 
-// собираем файл обратно
-const obj = nestie(flat);
-const xml = js2xml(obj, { spaces: 2 });
-
-// узнаём полный файл для будущего файла
-const pathObject = path.parse(filePath);
-pathObject.name += '-modified';
-delete pathObject.base;
-const newFilePath = path.format(pathObject);
-console.log('New file:', newFilePath);
-
-// записываем файл
-fs.writeFileSync(newFilePath, xml, 'utf-8');
 const elapsedTime = process.hrtime(startAt);
 console.log(`Done ${prettyHrtime(elapsedTime)}.`);
 
